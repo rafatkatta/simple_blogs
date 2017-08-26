fabfa27cf88a9c147a1a62bfd2ec0107140460b8
Rafat Katta <rafatkatta@gsibc.net>
2017-08-26 11:27:24 +0200

    Scope Bloges to User oder AdminUser

--- app/admin/blog.rb
+++ app/admin/blog.rb
@@ -3,5 +3,15 @@ ActiveAdmin.register Blog do
     #belongs_to :category
     permit_params :name, :description,  :user_id, :category_id
     scope_to :current_user, unless: proc{ current_user.admin? }
-
+    controller do
+      def scoped_collection
+        if current_user.is_admin?
+          Blog.all
+        else
+          Blog.where(user_id: current_user.id)        
+        end
+      end
+    end  
+  
+    
 end
